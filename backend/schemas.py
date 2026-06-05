8dd182de9802f60219e2da1ee34a9686e10b78dc
dharani25007-code <dharani25007@gmail.com>
2026-06-05 06:19:59 +0530

    schemas.py  file

---
+++ backend/schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from enum import Enum
+
+class UserRole(str, Enum):
+    jobseeker = "jobseeker"
+    recruiter = "recruiter"
+
+class UserRegister(BaseModel):
+    name: str
+    email: str
+    password: str
+    role: UserRole
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str
+    role: str
+    name: str
+    user_id: int
